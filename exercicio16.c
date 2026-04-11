090e705b77b86b08330fad9b67db6519232a1222
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-10 23:06:36 -0300

    Término do exercício 16

--- exercicio16.c
+++ exercicio16.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void){
+    //Variáveis
     int temperatura;
 
+    //Entrada de dados
     printf("Digite a temperatura: ");
     scanf("%d", &temperatura);
 
+    //Processamento e saída de dados
     if (temperatura >= 1000){
         printf("Valor invalido (deve ser menor que 1000)\n");
     }
     else if (temperatura <= 500){
         printf("Temperatura Invalida\n");
+    }else if (temperatura > 780){
+        printf("SUPERAQUECIMENTO\n");
     }
+    else if (temperatura >= 735){
+        printf("Aquecimento Desligado\n");
+    }
+    else if (temperatura >= 700){
+        printf("Aquecimento Ligado em 50%%\n");
+    }
+    else { 
+        printf("Aquecimento Ligado em 100%%\n");
+    }
+
+    return 0;
+}
     
