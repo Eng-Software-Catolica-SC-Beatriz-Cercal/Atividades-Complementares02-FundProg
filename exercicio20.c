28ea7b151a96f9d3815fdc4402527d3f0f5d831d
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-10 23:44:31 -0300

    Exercício 20 finalizado

--- exercicio20.c
+++ exercicio20.c
@@ -25,6 +25,27 @@ int main(void){
         bissexto = 1;
     }
 
-   
+    // Define dias do mês
+    if (mes == 1 || mes == 3 || mes == 5 || mes == 7 ||
+        mes == 8 || mes == 10 || mes == 12){
+        diasNoMes = 31;
+    }
+    else if (mes == 4 || mes == 6 || mes == 9 || mes == 11){
+        diasNoMes = 30;
+    }
+    else { // fevereiro
+        if (bissexto)
+            diasNoMes = 29;
+        else
+            diasNoMes = 28;
+    }
+
+    // Validação do dia
+    if (dia < 1 || dia > diasNoMes){
+        printf("Data invalida\n");
+    } else {
+        printf("Data valida\n");
+    }
+
     return 0;
 }
