5bb8636cfa36f4a2e03a212e570ca0e9cb6bf50c
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-11 16:39:42 -0300

    Exercício 22 finalizado

--- exercicio22.c
+++ exercicio22.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    //Variáveis
+int main(void)
+{
+    // Variáveis
     int tipo, opcao1, opcao2;
 
-    //Entrada de dados
+    // Entrada de dados
     printf("Escolha o tipo:\n");
     printf("1 - Mamiferos;\n");
     printf("1 - Aves;\n");
@@ -13,8 +14,9 @@ int main(void){
     scanf("%d", &tipo);
     fflush(stdin);
 
-    //Entrada de dados
-    if(tipo == 1){
+    // Entrada de dados
+    if (tipo == 1)
+    {
         printf("[1] Quadrupede\n");
         printf("[2] Bipede\n");
         printf("[3] Voador\n");
@@ -22,37 +24,91 @@ int main(void){
         scanf("%d", &opcao1);
         fflush(stdin);
 
-        //Processamento e saída de dados
-        if(opcao1 == 1){
+        // Processamento e saída de dados
+        if (opcao1 == 1)
+        {
             printf("[1] Carnivoro\n");
             printf("[2] Herbivoro\n");
             scanf("%d", &opcao2);
             fflush(stdin);
 
-            if(opcao2 == 1)
+            if (opcao2 == 1)
                 printf("Animal: Leao\n");
-            else if(opcao2 == 2)
+            else if (opcao2 == 2)
                 printf("Animal: Cavalo\n");
         }
-        else if(opcao1 == 2){
+        else if (opcao1 == 2)
+        {
             printf("[1] Onivoro\n");
             printf("[2] Frutifero\n");
             scanf("%d", &opcao2);
             fflush(stdin);
 
-            if(opcao2 == 1)
+            if (opcao2 == 1)
                 printf("Animal: Homem\n");
-            else if(opcao2 == 2)
+            else if (opcao2 == 2)
                 printf("Animal: Macaco\n");
         }
-        else if(opcao1 == 3){
+        else if (opcao1 == 3)
+        {
             printf("Animal: Morcego\n");
         }
-        else if(opcao1 == 4){
+        else if (opcao1 == 4)
+        {
             printf("Animal: Baleia\n");
         }
-    }
+        else if (tipo == 2)
+        {
+            printf("[1] Nao voadoras;\n");
+            printf("[2] Nadadoras;\n");
+            printf("[3] De rapina.\n");
+            scanf("%d", &opcao1);
+            fflush(stdin);
 
-  
-    return 0;
+            if (opcao1 == 1)
+            {
+                printf("[1] Tropical\n");
+                printf("[2] Polar\n");
+                scanf("%d", &opcao2);
+                fflush(stdin);
+
+                if (opcao2 == 1)
+                    printf("Animal: Avestruz\n");
+                else if (opcao2 == 2)
+                    printf("Animal: Pinguim\n");
+            }
+            else if (opcao1 == 2)
+            {
+                printf("Animal: Pato\n");
+            }
+            else if (opcao1 == 3)
+            {
+                printf("Animal: Aguia\n");
+            }
+        }
+
+        // Processamento e saída de dados
+        else if (tipo == 3)
+        {
+            printf("[1] Com casco;\n");
+            printf("[2] Carnivoros\n");
+            printf("[3] Sem patas.\n");
+            scanf("%d", &opcao1);
+            fflush(stdin);
+
+            if (opcao1 == 1)
+                printf("Animal: Tartaruga\n");
+            else if (opcao1 == 2)
+                printf("Animal: Crocodilo\n");
+            else if (opcao1 == 3)
+                printf("Animal: Cobra\n");
+        }
+
+        else
+        {
+            printf("Opcao invalida!\n");
+        }
+
+        return 0;
+    }
 }
